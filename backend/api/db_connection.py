7f2383bf117fc0589a6a20da2d7d1e72ca32e458
ViralBiyawala <ui21cs66@iiitsurat.ac.in>
2024-04-03 20:55:43 +0530

    backend

--- backend/api/db_connection.py
+++ backend/api/db_connection.py
@@ -1,10 +1,4 @@
-# import pymongo
-
-# MONGODB_CONNECTION_URL = "mongodb://localhost:27017/"
 MONGODB_DATABASE_NAME = "mha"
-
-# client = pymongo.MongoClient(MONGODB_CONNECTION_URL)
-# db = client[MONGODB_DATABASE_NAME]
 
 
 from pymongo.mongo_client import MongoClient
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,19 +1,37 @@
 from django.shortcuts import render
 from .models import per_con
-from django.http import HttpResponse
+from django.http import HttpResponse,JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
 
 # Create your views here.
 def index(request):
   return HttpResponse("<h1>Running</h1>")
 
+@csrf_exempt
 def add_person(request):
-  records ={
-    "first_name":"Viral",
-    "last_name":"Biyawala",
-  }
-  per_con.insert_one(records)
-  return HttpResponse("New record!")
+  if request.method == 'POST':
+    data = json.loads(request.body)
+    first_name = data.get('first_name', '')
+    last_name = data.get('last_name', '')
+    records = {
+      'first_name': first_name,
+      'last_name': last_name
+    }
+    if first_name and last_name:
+      per_con.insert_one(records)
+    else:
+      return JsonResponse({'error': 'Missing data'}, status=400)
+  else:
+    return JsonResponse({'error': 'Only POST requests are allowed'}, status=405)
 
 def get_all_person(request):
   persons = per_con.find()
-  return HttpResponse(persons)
+  parsed_persons = []
+  
+  for person in persons:
+      # Convert ObjectId to string
+      person['_id'] = str(person['_id'])
+      parsed_persons.append(person)
+      
+  return JsonResponse(parsed_persons, safe=False)
